5d45fe0b3d593d8609a01dfd2b1798619e8b45b0
SumanSantosh <72086164+SumanSantosh@users.noreply.github.com>
2021-09-05 20:52:40 +0530

    Update guessingGame.py

--- guessingGame.py
+++ guessingGame.py
@@ -9,26 +9,22 @@ chances_left = 5
 print("Hey there! Guess a number between 1 to 10 which I am thinking of: ")
 
 
-for i in range(1, 10):
-  
-  guess = int(input("Take a guess: "))
-  
+while chances_left > 0:
 
-  if guess < secret_number:
-    print("Guess is too low!")
-  elif guess > secret_number:
-    print("Guess is too high!")
-  else:
-    
-    break
+    guess = int(input("Take a guess: "))
 
-  chances_left = chances_left - 1
-  print("Chances Left: " + str(chances_left))
+    if guess == secret_number:
+        print("You guessed it right!")
+
+    elif guess < secret_number:
+        print("Guess is too low!")
+
+    elif guess > secret_number:
+        print("Guess is too high!")
+
+    chances_left = chances_left - 1
+    print("Chances Left: " + str(chances_left))
 
 
-
-if guess == secret_number:
-  print("You guessed it right!")
-else:
-  print("You lose!")
-  print("The number I thought was " + str(secret_number))
+if chances_left == 0:
+    print("You have lost the game")
